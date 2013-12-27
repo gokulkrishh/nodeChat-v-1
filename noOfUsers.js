1396577340dd5ac7e0641c0a75beafe1adf3d511
gokulkrishh <krishnangokul9@gmail.com>
2013-12-27 23:17:18 +0530

    Socket.io no of users connected

---
+++ noOfUsers.js
@@ -0,0 +1,36 @@
+var express = require('express') //required express module
+  , app = express()
+  , server = app.listen(1337); //creating a server with express using http module ref
+var io = require('socket.io').listen(server);//required socket.io module which listen to http server
+
+/* this and above is same
+var express = require(“express”);
+var app = express();
+var socket = require(‘socket.io’);
+var server = app.listen(4000);
+var io = socket.listen(server); */
+
+app.get('/',function(req,res) 
+{
+	res.sendfile(__dirname + '/Users.html');// send a file in current dir that is index.html
+	//console.log(http.createServer(app));
+});
+var activeClients = 0;
+ 
+io.sockets.on('connection', function(socket) // initialize connection from client and socket arg is used for further
+{
+	clientConnect(socket); //socker arg is passed to funz
+}); 
+ 
+function clientConnect(socket){
+  activeClients +=1;
+  io.sockets.emit('message', {clients:activeClients}); //will emit the msg
+
+  socket.on('disconnect', function(){clientDisconnect()});
+}
+ 
+function clientDisconnect(){
+  activeClients -=1;
+  io.sockets.emit('message', {clients:activeClients});
+}
+
