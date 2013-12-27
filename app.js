6cf2725b6aa00162f977ee5a7833d429fa0635a9
gokulkrishh <krishnangokul9@gmail.com>
2013-12-27 21:32:11 +0530

    created route

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var port = obj.port;
 app.get('/',function(req,res) 
 {
 	res.sendfile(__dirname + '/index.html');// send a file in current dir that is index.html
-	//console.log(__dirname+'/index.html');
 });
 app.listen(port,host);
 console.log("Listening to "+host+":"+port);
