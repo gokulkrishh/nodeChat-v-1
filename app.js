467e2fbe7b9f0ff883d97756267ad68006fae5d9
gokulkrishh <krishnangokul9@gmail.com>
2013-12-28 15:25:12 +0530

    added chatbox

--- app.js
+++ app.js
@@ -10,4 +10,28 @@ var socket = require(‘socket.io’);
 var server = app.listen(4000);
 var io = socket.listen(server); */
 
-
+app.get('/',function(req,res)
+{
+	res.sendfile(__dirname + '/index.html');
+	//console.log(__dirname);
+});
+app.get('/images',function(req,res)
+{
+	res.send(__dirname + '/images');
+});
+io.sockets.on("connection",function(socket) 
+{
+	//console.log(socket);
+	socket.on("userMsg",function(content) //listening to user msg userMsg
+	{
+		//io.sockets.emit("updateMsg","A user joined chat");
+		io.sockets.emit("updateMsg",content); //sending msg to only user who connected to that socket
+		//console.log(content);// it will return user emitted msg
+		socket.on("disconnect",function(){clientDisconnect(content)});
+	});
+});
+//disconnect funz. if user left the connection the below msg will come
+function clientDisconnect(content) 
+{
+	io.sockets.emit("leftMsg",content.username + ' left chat');
+}
